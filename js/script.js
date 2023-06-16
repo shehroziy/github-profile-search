e903d582d04d0e5f25541fdf068f635da6a37f2a
Shehroz <116956168+shehroziy@users.noreply.github.com>
2023-06-16 10:42:14 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const getUser = async (username) => {
     const resData = await res.json();
     createUser(resData)
 }
-getUser("rshehroz1")
+getUser("shehroziy")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
